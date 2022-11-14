7ad65313ba718448c463a86c91aee039e0f8db43
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-14 22:05:25 +0200

    feat: create Wrapper component and implement it to the entry point

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '6.5xl': '78rem',
+        19: '19.375rem',
+      },
+    },
   },
   plugins: [],
-}
+};
