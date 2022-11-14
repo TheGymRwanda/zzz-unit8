fff504f6d191d65b10cf77d6df411a0c2b419806
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-11-14 18:27:17 +0200

    chore: initialization
    adds project initialization with nextjs
    configurate tailwindcss setup
    renders well done text to home page to test tailwind
    
    #done

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className='h-screen bg-slate-900 text-9xl font-extrabold flex items-center justify-center text-center text-blue-500 uppercase'>
+      <p>
+        we are almost here...
+      </p>
+    </div>
+  )
+}
