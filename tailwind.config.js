9c92e7e521d5d7576da6daa4996ad09e9ce40d8a
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-15 11:42:05 +0200

    feat: rebase gather assets branch continue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                grotesk: ["Grotesk", "sans-serif"],
-            },
-            colors: {
-                'primary':{
-                  '300': "#5602E0",
-                },
-                'muted':{
-                  '300': '#9A9A9A'
-                }
-            },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        grotesk: ["Grotesk", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          300: "#5602E0",
         },
+        muted: {
+          300: "#9A9A9A",
+        },
+      },
+      maxWidth: {
+        "6.5xl": "78rem",
+        19: "19.375rem",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
