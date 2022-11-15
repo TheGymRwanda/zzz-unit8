a6e090f33168ea1e304e75e0573d498f81aa56bc
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-11-15 19:56:41 +0200

    feat: adds header tablet break point
    fix hydrant issues
    fix container issues
    
    #done

--- components/ui/CustomLink.jsx
+++ components/ui/CustomLink.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 export default function CustomLink({ link , text , style }) {
     return (
         <Link href={`${link ? link : "/"}`}>
-            <span className={`font-normal group transition-all duration-300 text-xl leading-7 relative inline-flex items-center hover:underline  ${style}`}>
+            <div className={`font-normal group transition-all duration-300 text-xl leading-7 relative inline-flex items-center hover:underline  ${style}`}>
                 <span className="self-end ">
                     {text}
                 </span>
-                <div className=" absolute -right-4 top-0.6 group-hover:top-0.2  lg:group-hover:top-0.3 group-hover:-right-5">
-                    <Image src={'/assets/icons/upRight_arrow.svg'} width={"12"} height={"12"} alt={"Up right icon"} />
+                <div className={`absolute -right-4 top-0.6 group-hover:top-0.2  lg:group-hover:top-0.3 group-hover:-right-5`}>
+                    <Image src={'/assets/icons/upRight_arrow.svg'} width={"12"} height={"12"} alt="Up right icon" />
                 </div>
-            </span>
+            </div>
         </Link >
     )
 }
