411406d2b4804a8b84a762ba90da3d7346c5d4df
serapieTuyishime <70019756+serapieTuyishime@users.noreply.github.com>
2022-11-15 04:13:37 -0800

    chore:  gathering images and font
    * feat: gathered images and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
-  return (
-    <div className='h-screen text-xl font-extrabold text-blue-500 uppercase text-start'>
-      <p>we are almost here...</p>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center h-screen font-extrabold text-center text-blue-500 uppercase bg-slate-900 text-9xl">
+            <p>we are almost here...</p>
+        </div>
+    );
+
 }
