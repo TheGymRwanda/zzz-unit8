147a5770274aaec0a6f1d852bfdc8f6b1d1dae8b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-15 20:54:15 +0200

    feat: apply the request change

--- components/sections/Project.jsx
+++ components/sections/Project.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { projectData } from "../../utils/projectData";
 
 export default function Project() {
   const [shown, setShown] = useState(0);
-  const hclik = (e) => {
+  const shownImageOnclik = (e) => {
     setShown(e.target.id);
   };
   return (
-    <div className='flex flex-col'>
-      <h4 className='text-xl text-muted-300'>Projects</h4>
+    <div className="flex flex-col">
+      <h4 className="text-xl text-muted-300">Projects</h4>
 
       {projectData.map((items) => {
         const { id, image, title, subtitle } = items;
@@ -19,13 +19,13 @@ export default function Project() {
             <h3
               id={id}
               className={`text-5.5xl leading-12 mb-4`}
-              onClick={hclik}
+              onClick={shownImageOnclik}
             >
               {title}
             </h3>
             <div className={`${id === parseInt(shown) ? "" : "hidden"}`}>
-              <Image height='191' width='349' src={image} alt='project-image' />
-              <p className='mt-2.5 text-xl leading-7 mb-12'>{subtitle}</p>
+              <Image height="191" width="349" src={image} alt="project-image" />
+              <p className="mt-2.5 text-xl leading-7 mb-12">{subtitle}</p>
             </div>
           </div>
         );
