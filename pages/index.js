240a0a331b61b31d3161841d597b0b3d9a67c4f5
Ibrahim Bagalwa <67462827+IbrahimBagalwa@users.noreply.github.com>
2022-11-15 10:59:36 +0100

    feat: adds content wrapper (#1)
    * feat: create Wrapper component and implement it to the entry point
    
    * feat: create Wrapper component and implement it to the entry point
    
    * feat: create Wrapper component and implement it to the entry point
    
    * refactor: wrapper content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-
 export default function Home() {
   return (
-    <div className='h-screen bg-slate-900 text-9xl font-extrabold flex items-center justify-center text-center text-blue-500 uppercase'>
-      <p>
-        we are almost here...
-      </p>
+    <div className='h-screen text-xl font-extrabold text-blue-500 uppercase text-start'>
+      <p>we are almost here...</p>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "8xl": "82rem",
+      },
+    },
   },
   plugins: [],
-}
+};
