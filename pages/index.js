612c8ecae57476837630b25c61b566fe567914cf
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-15 11:39:26 +0200

    feat: rebase gather assets branch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <div className="flex items-center justify-center h-screen font-extrabold text-center text-blue-500 uppercase bg-slate-900 text-9xl">
-            <p>we are almost here...</p>
-        </div>
-    );
+  return (
+    <div className='flex items-center justify-center h-screen font-extrabold text-center text-blue-500 uppercase bg-slate-900 text-9xl'>
+      <p>we are almost here...</p>
+    </div>
+  );
 }
