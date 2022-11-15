53edac126f2a93a27ecb4aed90cbbdc541d56f79
serapieTuyishime <tuyiserapie1@gmail.com>
2022-11-15 16:39:00 +0200

    feat: created about us section wil click effects

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import AboutUs from "../components/sections/AboutUs";
+
 export default function Home() {
-    return (
-        <div className="flex items-center justify-center h-screen font-extrabold text-center text-blue-500 uppercase bg-slate-900 text-9xl">
-            <p>we are almost here...</p>
-        </div>
-    );
+    return <AboutUs />;
 }
