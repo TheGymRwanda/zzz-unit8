f480d056623b06709ed78f105d6229e6409021c6
serapieTuyishime <70019756+serapieTuyishime@users.noreply.github.com>
2022-11-15 04:13:37 -0800

    chore:  gathering images and font
    * feat: gathered images and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      maxWidth: {
-        "8xl": "82rem",
-      },
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                grotesk: ["Grotesk", "sans-serif"],
+            },
+            colors: {
+                primary: {
+                    300: "#5602E0",
+                },
+                muted: {
+                    300: "#9A9A9A",
+                    200: "#878787",
+                    100: "#E2E2E2",
+                    50: "#F4F4F4",
+                },
+            },
+            maxWidth: {
+                "8xl": "82rem",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
+
 };
