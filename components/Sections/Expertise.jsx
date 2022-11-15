72f4ae1aa711c56d809a7073bbdc7d8ff9d55f7e
ivad <yvesyvad@gmail.com>
2022-11-15 15:40:38 +0200

    feat: add expertise list styles

--- components/Sections/Expertise.jsx
+++ components/Sections/Expertise.jsx
@@ -1,3 +1,31 @@
 export default function Expertise() {
-  return <section>Expertise</section>;
+  const expertiseList = [
+    'Product Development',
+    'Product Design',
+    'Community management',
+    'Strategy',
+    'Governance',
+    'Product Design',
+    'Product Development',
+    'Product Design',
+    'Product Development',
+    'Product Design',
+  ];
+  return (
+    <section className='float-right'>
+      <div className='-tracking-0.01'>
+        <h4 className='text-2xl text-muted-300'>Expertise</h4>
+        <div className='grid grid-cols-1 mt-5 gap-y-3'>
+          {expertiseList.map((expertise, index) => (
+            <h5
+              key={index}
+              className='text-xl leading-8 border-b border-muted-100 w-70w'
+            >
+              {expertise}
+            </h5>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
