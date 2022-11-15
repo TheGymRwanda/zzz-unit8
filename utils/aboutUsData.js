91e140165321953095f2424c0572b891e56f1e03
serapieTuyishime <tuyiserapie1@gmail.com>
2022-11-15 21:35:12 +0200

    refactor: About us data now in a separate file

---
+++ utils/aboutUsData.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        title: "About us",
+        paragraphs: [
+            "Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. Learn more",
+            "Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.",
+        ],
+    },
+    {
+        title: "What we do",
+        paragraphs: [
+            "Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. Learn more",
+            "Work Meaningful - With our work, we aim to impact society positively. The projects we get involved with the aim to solve significant problems in our society, sustainably and in the long run, rather than looking for a quick profit through speculation or encouraging harmful behavior.",
+        ],
+    },
+    {
+        title: "Employment",
+        paragraphs: [
+            "Blockchain will do for cooperation what the internet did for communication - We believe that blockchain technology has the potential to shift financial models as we know them, creating revenue streams that are more sustainable and fair for users. We envision a future in which financial infrastructures are rather decentralised and horizontal, in which any member can have a say. Through its immutability, blockchain technology provides trustworthy infrastructures that allow cooperation on a large scale. Learn more",
+        ],
+    },
+];
