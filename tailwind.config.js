5b1fda28d4fb14ed1f976faa26473c18d308f342
ivad <yvesyvad@gmail.com>
2022-11-15 15:40:38 +0200

    feat: add expertise list styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                grotesk: ["Grotesk", "sans-serif"],
-            },
-            colors: {
-                primary: {
-                    300: "#5602E0",
-                },
-                muted: {
-                    300: "#9A9A9A",
-                    200: "#878787",
-                    100: "#E2E2E2",
-                    50: "#F4F4F4",
-                },
-            },
-            maxWidth: {
-                "8xl": "82rem",
-            },
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        grotesk: ['Grotesk', 'sans-serif'],
+      },
+      maxWidth: {
+        '8xl': '82rem',
+      },
+      colors: {
+        primary: {
+          300: '#5602E0',
         },
+        muted: {
+          300: '#9A9A9A',
+          200: '#878787',
+          100: '#E2E2E2',
+          50: '#F4F4F4',
+        },
+      },
+      maxWidth: {
+        '8xl': '82rem',
+      },
+      width: {
+        '70w': '70vw',
+      },
+      letterSpacing: {
+        0.01: '0.01rem',
+      },
     },
-    plugins: [],
-
+  },
+  plugins: [],
 };
