a1304ff0bd5e7ad72daa894ba38caccca71dc2fb
serapieTuyishime <tuyiserapie1@gmail.com>
2022-11-15 19:05:41 +0200

    chore: putting wrapper, header, expertise and about us together

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        // <div className='flex items-center justify-center h-screen font-extrabold text-center text-blue-500 uppercase bg-slate-900 text-9xl'>
+        //   <p>
+        //     we are almost here...
+        //   </p>
+        // </div>
+        <div>
+            Ape Unit Unit➇ is a pioneering technology company specialing in
+            decentralised technologies that creates end-to-end digital
+            experiences for protocols including Ethereum, Tezos, Near, Algorand,
+            Celo and more. Its team of 2,500+ digital specialists across 30+
+            locations on 5 continents delivers pioneering work on a global scale
+            with a boutique culture.
+        </div>
+    );
+}
