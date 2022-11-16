ac081343312a57001bdfcfa77c6a2bf3b07090ca
ivad <yvesyvad@gmail.com>
2022-11-16 19:06:20 +0200

    feat: make links open in a separate window

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -3,27 +3,42 @@ import CustomLink from "../ui/CustomLink";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col text-xl gap-11.1 -tracking-0.01 md:text-2xl md:flex-row md:justify-between mb-11.1">
-      <address className="not-italic">
-        Ape Unit GmbH <br /> Waldemarstraße 38, <br /> 10999 Berlin
-      </address>
-      <div className="flex flex-col">
-        <CustomLink link="https://twitter.com/apeunit" text="Twitter" />
-        <CustomLink link="https://www.facebook.com/apeunit/" text="Facebook" />
-        <CustomLink
-          link="https://de.linkedin.com/company/ape-unit"
-          text="LinkedIn"
-        />
+    <footer>
+      <div className="flex flex-col text-xl gap-11.1 -tracking-0.01 md:text-2xl md:flex-row md:justify-between mb-11.1">
+        <address className="not-italic">
+          Ape Unit GmbH <br /> Waldemarstraße 38, <br /> 10999 Berlin
+        </address>
+        <div className="flex flex-col">
+          <CustomLink
+            link="https://twitter.com/apeunit"
+            text="Twitter"
+            target="_blank"
+          />
+          <CustomLink
+            link="https://www.facebook.com/apeunit/"
+            text="Facebook"
+            target="_blank"
+          />
+          <CustomLink
+            link="https://de.linkedin.com/company/ape-unit"
+            text="LinkedIn"
+            target="_blank"
+          />
+        </div>
+        <h4>
+          <span>
+            <Link href="https://apeunit.com/impressum.txt" target="_blank">
+              Impressum
+            </Link>
+          </span>{" "}
+          /{" "}
+          <span>
+            <Link href="https://apeunit.com/datenschutz.txt" target="_blank">
+              Privacy
+            </Link>
+          </span>
+        </h4>
       </div>
-      <h4>
-        <span>
-          <Link href="https://apeunit.com/impressum.txt">Impressum</Link>
-        </span>{" "}
-        /{" "}
-        <span>
-          <Link href="https://apeunit.com/datenschutz.txt">Privacy</Link>
-        </span>
-      </h4>
     </footer>
   );
 }
