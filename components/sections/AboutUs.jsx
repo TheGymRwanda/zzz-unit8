b49d146e5f2e499f25e53ba5906e3673e4f7e526
serapieTuyishime <tuyiserapie1@gmail.com>
2022-11-21 11:11:58 +0200

    refractor: Removed unused configurations and added more meaningful ones in tailiwing.config.js

--- components/sections/AboutUs.jsx
+++ components/sections/AboutUs.jsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
 import { useState } from "react";
-import CustomLink from "../ui/CustomLink";
+import { Tab } from "@headlessui/react";
 import { data } from "/utils/aboutUsData";
+import CustomLink from "../ui/CustomLink";
+import UpArrow from "../vectors/UpArrow";
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
 
 export default function AboutUs() {
     const [selectedDiv, setSelectedDiv] = useState(0);
@@ -9,49 +14,22 @@ export default function AboutUs() {
         if (selectedDiv === id) return setSelectedDiv(null);
         setSelectedDiv(id);
     }
-
     return (
-        <div className="">
+        <div>
             <div className="grid w-full gap-6 mb-6.6 text-xl md:hidden ">
                 {data.map((el, index) => {
                     let { title, paragraphs } = el;
                     return (
                         <div className="grid gap-4" key={index}>
                             <div
-                                className={`flex items-center justify-between border-b cursor-pointer border-b-black -tracking-hug ${
-                                    selectedDiv === index && "text-primary-300 border-b-primary-300"
+                                className={`flex items-center justify-between border-b cursor-pointer border-b-black -tracking-lg ${
+                                    selectedDiv === index &&
+                                    "text-primary-300 border-b-primary-300"
                                 }`}
                                 onClick={() => changeVisibility(index)}
                             >
                                 {title}
-
-                                {/*  Dev decided to use an SVG because they want to change the color dynamically */}
-                                <svg
-                                    width="16"
-                                    height="17"
-                                    viewBox="0 0 16 17"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className={`duration-300 ${
-                                        selectedDiv === index
-                                            ? "rotate-180 text-primary-300"
-                                            : "rotate-0"
-                                    }`}
-                                >
-                                    <path
-                                        d="M13.9365 9.21997L7.4518 15.7047L0.967072 9.21997"
-                                        stroke="currentColor"
-                                        strokeWidth="1.5"
-                                    />
-                                    <line
-                                        x1="7.44336"
-                                        y1="15.7045"
-                                        x2="7.44336"
-                                        y2="0.141126"
-                                        stroke="currentColor"
-                                        strokeWidth="1.5"
-                                    />
-                                </svg>
+                                <UpArrow color={selectedDiv === index} />
                             </div>
                             <div
                                 className={`gap-6 ${
@@ -65,53 +43,52 @@ export default function AboutUs() {
                     );
                 })}
             </div>
-            <div className="flex w-full justify-end md:min-h-96 flex-grow-0">
+            <div className="flex justify-end flex-grow-0 w-full md:min-h-96">
                 <div className="md:flex md:flex-col gap-6 mb-6.6 hidden w-full text-2xl xl:w-50">
-                    <div className="flex h-8 gap-6 text-muted-300 -tracking-hug ">
-                        {data.map((el, index) => {
-                            return (
-                                <div
-                                    className={` relative group overflow-hidden   transition duration-300 pb-1 cursor-pointer ${
-                                        selectedDiv == index
-                                            ? "text-primary-300 "
-                                            : " "
-                                    }`}
+                    <Tab.Group defaultIndex={0}>
+                        <Tab.List className="flex h-8 gap-6 text-muted-300 -tracking-lg ">
+                            {data.map((el, index) => (
+                                <Tab
                                     key={index}
-                                    onClick={() => setSelectedDiv(index)}
+                                    className={({ selected }) =>
+                                        classNames(
+                                            "outline-none after:opacity-0 transition-all duration-500 after:bg-primary-300 after:w-full after:h-0.1 after:transition-all after:duration-500 after:ease-in after:absolute after:bottom-0 ",
+                                            "relative group overflow-hidden   transition duration-300 pb-1 cursor-pointer",
+                                            selected
+                                                ? "after:!left-0 after:opacity-100 text-primary-300 "
+                                                : " "
+                                        )
+                                    }
                                 >
-                                    <div className={` after:opacity-0 transition-all duration-500 after:transition-all after:duration-500 after:bg-primary-300 after:w-full after:h-0.1 after:absolute after:bottom-0 after:-left-full   ${selectedDiv == index
-                                            ? " after:!left-0 after:opacity-100 "
-                                            : " "}`}>
-                                        {el.title}
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div className="grid -tracking-0.01 gap-6 relative">
-                        {data.map((el, index) => {
-                            return (
-                                <div
-                                    key={index}
-                                    className={`absolute grid gap-4 transition duration-500 ${
-                                        selectedDiv === index
-                                            ? "text-black/100 relative"
-                                            : "text-black/0 select-none"
-                                    }`}
+                                    {el.title}
+                                </Tab>
+                            ))}
+                        </Tab.List>
+                        <Tab.Panels className="grid gap-6 mt-2 -tracking-lg">
+                            {data.map((el, idx) => (
+                                <Tab.Panel
+                                    key={idx}
+                                    className={classNames(
+                                        "grid gap-4 transition duration-500"
+                                    )}
                                 >
-                                    <div>
-                                        {el.paragraphs[0]}{" "}
-                                        <CustomLink
-                                            link={el.link}
-                                            text="Learn more"
-                                            style="text-2xl"
-                                        />
+                                    <div
+                                        className={`gap-6 grid transition-all  duration-700`}
+                                    >
+                                        <div>
+                                            {el.paragraphs[0]}{" "}
+                                            <CustomLink
+                                                link={el.link}
+                                                text="Learn more"
+                                                style="text-2xl"
+                                            />
+                                        </div>
+                                        <div>{el.paragraphs[1] || " "}</div>
                                     </div>
-                                    <div>{el.paragraphs[1] || " "}</div>
-                                </div>
-                            );
-                        })}
-                    </div>
+                                </Tab.Panel>
+                            ))}
+                        </Tab.Panels>
+                    </Tab.Group>
                 </div>
             </div>
         </div>
