83a53974b61211eb0c0b3dd79cb55ffb43ed93e3
ivad <yvesyvad@gmail.com>
2022-11-21 09:57:29 +0200

    bg: update Expertise width

--- components/sections/Expertise.jsx
+++ components/sections/Expertise.jsx
@@ -6,10 +6,9 @@ export default function Expertise() {
         <h4 className=" text-muted-300">Expertise</h4>
         <div className="grid grid-cols-1 mt-5 md:grid-cols-2 gap-y-3 gap-x-6">
           {expertiseList.map((expertise, index) => (
-            // used width of 70vw (w-70w) to intentionaly flow the text to 70% of the whole screen (wrapper margin included) but only on mobile screens below 400px of width
             <h5
               key={index}
-              className="border-b border-muted-100 w-70w ssm:w-73 lg:w-74"
+              className="border-b border-muted-100 w-full ssm:w-73 lg:w-74"
             >
               {expertise}
             </h5>
