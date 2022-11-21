984addebea995cf045422ae462c5642a42c9cf42
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-11-21 19:06:53 +0200

    refactor: applies QA request

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -3,11 +3,11 @@ import CustomLink from "../ui/CustomLink";
 export default function Header() {
     return (
         <section className="my-6.6  w-full">
-            <div className="flex gap-5.5 flex-col min-w-full text-2.5xl font-normal leading-7 xl:flex-row lg:justify-between lg:text-2xl -tracking-sm">
+            <div className="flex gap-5.5 flex-col min-w-full text-2.5xl font-normal leading-7 lg:flex-row lg:justify-between sm:text-2xl -tracking-sm">
                 <h1 className="flex-grow">
                     Ape Unit
                 </h1>
-                <span className="xl:w-50">
+                <span className="lg:w-9/12 xl:w-50">
                     Unit➇ is a pioneering technology company specialing in 
                     decentralised technologies that creates end-to-end digital 
                     experiences for protocols including 
--- components/ui/CustomLink.jsx
+++ components/ui/CustomLink.jsx
@@ -3,20 +3,15 @@ import Link from "next/link";
 
 export default function CustomLink({ link, text, style, target }) {
   return (
-      <Link href={`${link ? link : "/"}`} target={target}>
+      <Link className="relative" href={`${link ? link : "/"}`} target={target}>
         <div
-          className={`relative transition-all duration-700 inline-flex items-center lg:text-2xl text-2.5xl font-normal leading-7  group ${style}`}
+          className={`relative h-fit p-0 transition-all duration-700 inline-flex items-center lg:text-2xl text-2.5xl font-normal leading-7  group ${style}`}
         >
-          <span className="self-end -tracking-sm">{text}</span>
+          <span className="relative  transition after:duration-700 inline-flex items-center lg:text-2xl text-2.5xl font-normal leading-7 -tracking-sm after:content-['↗'] after:relative after:-bottom-1 after:text-2.5xl after:lg:text-2xl after:lg:group-hover:-top-1 after:lg:group-hover:-right-1 ">{text}</span>
           <span
-            className={`absolute transition-all duration-300 -right-4 top-0.6 group-hover:top-0.2 lg:group-hover:top-0.3 group-hover:-right-5`}
+            className={`absolute transition-all duration-300 -right-4 bottom-0 group-hover:top-0.2 lg:group-hover:top-0.3 group-hover:-right-5`}
           >
-            <Image
-              src={"/assets/icons/upRight_arrow.svg"}
-              width={"12"}
-              height={"12"}
-              alt="Up right icon"
-            />
+           
           </span>
         </div>
       </Link>
