926bb02c6d3b7d74d48d0791064cf2e7ac366170
ivad <yvesyvad@gmail.com>
2022-11-21 10:32:46 +0200

    refactor: add margin left to Expertise

--- components/sections/Expertise.jsx
+++ components/sections/Expertise.jsx
@@ -2,7 +2,7 @@ import expertiseList from "/utils/expertiseData";
 export default function Expertise() {
   return (
     <section className="flex justify-end mb-11.2 md:mb-33 lg:mb-24">
-      <div className="text-xl leading-8 -tracking-0.01 md:text-2xl">
+      <div className="text-xl leading-8 -tracking-0.01 md:text-2xl ml-7 ssm:ml-0">
         <h4 className=" text-muted-300">Expertise</h4>
         <div className="grid grid-cols-1 mt-5 md:grid-cols-2 gap-y-3 gap-x-6">
           {expertiseList.map((expertise, index) => (
