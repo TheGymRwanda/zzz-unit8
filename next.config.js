3726af675cb06169bf079c559a69b5ebc59614ee
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-11-21 08:15:40 +0200

    feat: fix deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
