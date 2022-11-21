367075b7bee22474a9bba41ec580b19886e13054
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2022-11-21 11:57:38 +0200

    bug: refactor header component
    Changes the header content
    Remove the underline
    
    #done

--- components/ui/CustomLink.jsx
+++ components/ui/CustomLink.jsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 
 export default function CustomLink({ link, text, style, target }) {
   return (
-    <Link href={`${link ? link : "/"}`} target={target}>
-      <div
-        className={`relative transition-all duration-700 inline-flex items-center text-xl font-normal leading-7  group underline hover:no-underline ${style}`}
-      >
-        <span className="self-end ">{text}</span>
+      <Link href={`${link ? link : "/"}`} target={target}>
         <div
-          className={`absolute  transition-all duration-300 -right-4 top-0.6 group-hover:top-0.2 lg:group-hover:top-0.3 group-hover:-right-5`}
+          className={`relative transition-all duration-700 inline-flex items-center lg:text-2xl text-2.5xl font-normal leading-7  group ${style}`}
         >
-          <Image
-            src={"/assets/icons/upRight_arrow.svg"}
-            width={"12"}
-            height={"12"}
-            alt="Up right icon"
-          />
+          <span className="self-end -tracking-sm">{text}</span>
+          <span
+            className={`absolute transition-all duration-300 -right-4 top-0.6 group-hover:top-0.2 lg:group-hover:top-0.3 group-hover:-right-5`}
+          >
+            <Image
+              src={"/assets/icons/upRight_arrow.svg"}
+              width={"12"}
+              height={"12"}
+              alt="Up right icon"
+            />
+          </span>
         </div>
-      </div>
-    </Link>
+      </Link>
   );
 }
